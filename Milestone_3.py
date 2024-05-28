f79f34667d57c1ca6475bc1cf6655a5b6349dcfc
Christian <ch.arenasdaza@gmail.com>
2024-05-28 13:55:52 +0100

    Add Milestone_2 code to file

---
+++ Milestone_3.py
@@ -0,0 +1,48 @@
+# Imports
+import random 
+
+# %%
+# List of words for hangman
+word_list = ["Mango", "Coconut", "Kiwi", "Orange", "Grapes"]
+print(word_list)
+
+
+# Random selector function
+def random_choice(word_list):
+    """ 
+    This fucntion selects a random word from a list of words.
+    
+    Arg:
+        word_list: list of words for the function to choose from.
+    
+    Returns:
+        str: random word choosen from the list.
+    """
+    random_word = random.choice(word_list)
+    return print(random_word)
+
+#%%
+# Get user imput
+def user_guess():
+    """ 
+    This function takes a single letter from user guess.
+
+    Args:
+        guess (str): take a single letter from the user.
+
+    Returns:
+        str: letter guess by the user
+    """
+    guess = input("Please enter a lette")
+
+    while True:
+        if guess.isalpha() == False or len(guess) != 1:
+            print("Oops! that is not a valid input, Please try again")
+            guess = input("Please enter a lette")
+        else:
+            break
+    print(f"{guess} is a good guess!")
+
+
+# %%
+user_guess()

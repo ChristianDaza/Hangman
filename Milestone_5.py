088e1d2e5c487aa086be2edaaa5e49355d89236e
Christian <ch.arenasdaza@gmail.com>
2024-05-30 08:07:18 +0100

    Copy Milestone_4.py into Milestone_5.py

---
+++ Milestone_5.py
@@ -0,0 +1,82 @@
+
+# %%
+#Class
+import random
+class  Hangman:
+     
+    def __init__(self, word_list:list[str], num_lives = 5):
+        self.word_list = word_list
+        self.num_lives = num_lives if num_lives != 5 else 5 
+        self.word = random.choice(word_list)
+        self.word_guessed = ["_"]*len(self.word)
+        self.num_letter = len(set(self.word))
+        self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        """" 
+        This function:
+             Checks if the user guess is in the randomly chosen word.
+             Saves the correct guess in a list with their correct guess.
+             Updates the number of ramaining lives after each guess.
+
+
+        
+        Returns: 
+            str: string confirming if the user guess was right or wrong.
+            lst: list with the correctly guessed letter, in their position correct positions.
+            int: number of remaining lives after each guess.
+        """
+
+        guess = guess.lower()
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word")
+            for i in self.word:
+                if i == guess:
+                    guess_index = self.word.index(i)
+                    self.word_guessed[guess_index] = guess
+            print(self.word_guessed)  
+        else:
+            print(f"Sorry, {guess} is not in the word.")
+            self.num_lives = self.num_lives -1
+            print(f"You have {self.num_lives} lives left.")
+
+    def ask_for_input(self):
+        """ 
+        This function:
+             Checks that the user guess is a single alphabetical character.
+             Pass the guess that fulfils the above conditions into the check_guess().
+
+
+        Args:
+            guess (str): take a single letter from the user.
+
+        Returns:
+            str: a string telling the user the guess is invalid and to try again.
+            str: a string telling the user that the letter had alrady being used for a previous guess.
+        """
+        
+        while True:
+            guess = input("Please enter a lette")
+            if guess.isalpha() == False or len(guess) != 1:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+                guess = input("Please enter a lette")
+                
+            elif guess in self.list_of_guesses:
+                print("This letter has already being used for a previous guess")
+                
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+                break
+# %%
+word_list_1 = ["mango", "coconut", "kiwi", "orange", "grapes"]
+hang_1 = Hangman(word_list_1, 8)
+print(hang_1.word)
+
+# %%
+hang_1.ask_for_input()
+# %%
+
+print(hang_1.num_lives)
+
+# %%
